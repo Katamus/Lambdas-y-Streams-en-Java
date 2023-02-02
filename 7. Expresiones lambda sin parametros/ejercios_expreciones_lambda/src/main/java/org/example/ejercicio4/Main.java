8c4b98656df827d5e52fd4415115849687c55159
Christian <christian.catamuscay.h@gmail.com>
2023-02-01 21:45:30 -0500

    11. Expresión lambda como retorno de métodos(Uso de las expresiones lambda)

---
+++ 7. Expresiones lambda sin parametros/ejercios_expreciones_lambda/src/main/java/org/example/ejercicio4/Main.java
@@ -0,0 +1,12 @@
+package org.example.ejercicio4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(create().calculate(2,2));
+    }
+
+    private static CalculadoraLong create(){
+        return (x,y) -> x / y;
+    }
+}
