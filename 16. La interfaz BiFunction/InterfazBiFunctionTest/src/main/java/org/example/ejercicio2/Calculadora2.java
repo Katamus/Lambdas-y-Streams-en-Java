850e0772d447136356e97914fc78154b77f6a867
Christian <christiancatamuscay@r-fast.com>
2023-02-02 11:13:32 -0500

    16. La interfaz BiFunction(Las interfaces funcionales)

---
+++ 16. La interfaz BiFunction/InterfazBiFunctionTest/src/main/java/org/example/ejercicio2/Calculadora2.java
@@ -0,0 +1,21 @@
+package org.example.ejercicio2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+
+public class Calculadora2 {
+
+    public List<Double> calc(BiFunction<Double,Double,Double> bi, List<Empleado> listaEmpleado, Double incremento){
+
+        List<Double> listaSalarios = new ArrayList<>();
+
+        for (Empleado empleado :
+        listaEmpleado) {
+            listaSalarios.add(bi.apply(empleado.getSalario(),incremento));
+        }
+        return listaSalarios;
+    }
+
+
+}
