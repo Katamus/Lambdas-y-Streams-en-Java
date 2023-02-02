9e05d938bc5fa84435bee4edab06d9ddc91520b3
Christian <christian.catamuscay.h@gmail.com>
2023-02-01 21:27:46 -0500

    9. Expresiones lambda con devolución(Uso de las expresiones lambda)

---
+++ 7. Expresiones lambda sin parametros/ejercios_expreciones_lambda/src/main/java/org/example/ejercicio3/Calculadora.java
@@ -0,0 +1,7 @@
+package org.example.ejercicio3;
+
+public interface Calculadora {
+
+    public int calcular(int x, int y );
+
+}
---
+++ 7. Expresiones lambda sin parametros/ejercios_expreciones_lambda/src/main/java/org/example/ejercicio3/Lambda3.java
@@ -0,0 +1,18 @@
+package org.example.ejercicio3;
+
+public class Lambda3 {
+
+    public static void main(String[] args) {
+        engine((x,y)-> x+y);
+        engine((x,y)-> x/y);
+        engine((x,y)-> x*y);
+        engine((x,y)-> x%y);
+    }
+
+    private  static void engine(Calculadora cal){
+        int x = 2, y = 4;
+        int resultado = cal.calcular(x,y);
+        System.out.println(resultado);
+    }
+
+}
