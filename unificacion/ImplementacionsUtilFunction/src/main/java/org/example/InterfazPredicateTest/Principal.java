66643cafa8061e4e9ab614f4cc5de8a227a4e47f
Christian <christiancatamuscay@r-fast.com>
2023-02-02 12:51:17 -0500

    17. La interfaz Predicate(Las interfaces funcionales)

---
+++ unificacion/ImplementacionsUtilFunction/src/main/java/org/example/InterfazPredicateTest/Principal.java
@@ -0,0 +1,47 @@
+package org.example.InterfazPredicateTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        List<Empleado> listaEmpleados = Arrays.asList(
+                new Empleado("Rodrigo",25,1500,"Cobranzas"),
+                new Empleado("Rodrigo",25,1500,"Cobranzas"),
+                new Empleado("Lidia",21,600,"Cobranzas"),
+                new Empleado("Eleazar",33,600,"Cobranzas"),
+                new Empleado("Javier",35,600,"Cobranzas"),
+                new Empleado("Paola",50,1500,"Cobranzas"),
+                new Empleado("Ignacio",41,1500,"Cobranzas"),
+                new Empleado("Rodrigo",40,7000,"Cobranzas")
+        );
+
+        Evaluador evaluador = new Evaluador();
+
+        System.out.println("Empleados con salarios > 5000");
+        List<Empleado> empSalariosAltos = evaluador.evaluar(listaEmpleados,
+                (empleado) -> empleado.getSalario() > 5000
+                );
+
+        for (Empleado empleado :empSalariosAltos ) {
+            System.out.println(empleado.getSalario());
+        }
+
+        System.out.println("Empleados que su nombre inicia con J: ");
+
+        List<Empleado> empIniNomJ = evaluador.evaluar(listaEmpleados,empleado -> empleado.getNombre().startsWith("J"));
+
+        for (Empleado empleado :empIniNomJ ) {
+            System.out.println(empleado.getNombre());
+        }
+
+        List<Empleado> empjove = evaluador.evaluar(listaEmpleados,empleado -> empleado.getEdad() < 25);
+        System.out.println("Empleados Jovenes : ");
+        for (Empleado empleado :empIniNomJ ) {
+            System.out.println(empleado.getNombre());
+        }
+
+    }
+
+}
