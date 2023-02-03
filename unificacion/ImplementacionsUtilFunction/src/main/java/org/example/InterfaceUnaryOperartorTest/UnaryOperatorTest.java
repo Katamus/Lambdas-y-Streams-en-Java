9ff5939feaebf22d028559cfc6d586004c99c77c
Christian <christiancatamuscay@r-fast.com>
2023-02-03 14:49:58 -0500

    23. La interfaz UnaryOperator(Las interfaces funcionales)

---
+++ unificacion/ImplementacionsUtilFunction/src/main/java/org/example/InterfaceUnaryOperartorTest/UnaryOperatorTest.java
@@ -0,0 +1,25 @@
+package org.example.InterfaceUnaryOperartorTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
+public class UnaryOperatorTest {
+
+    public static void main(String[] args) {
+        List<Integer> lista = Arrays.asList(
+                10,20,30,40,50,60,70,80,90,100
+        );
+        List<Integer> listaAct = operadorUnary(num->num*num,lista);
+        listaAct.forEach((x)->System.out.println(x));
+
+    }
+
+    public static List<Integer> operadorUnary(UnaryOperator<Integer> unarayOpt,List<Integer> lista){
+        List<Integer> listaActualizada = new ArrayList<>();
+        lista.forEach((num)-> listaActualizada.add(unarayOpt.apply(num)));
+        return listaActualizada;
+    }
+
+}
