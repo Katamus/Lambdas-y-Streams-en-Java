40d10243f5bca6aeff8d12a1a4a01126cf43ab57
Christian <christiancatamuscay@r-fast.com>
2023-02-03 14:32:17 -0500

    22. La interfaz Supplier-Las interfaces funcionales(Interfaces funcionales)

---
+++ unificacion/ImplementacionsUtilFunction/src/main/java/org/example/InterfaceSupplier/SupplierTest.java
@@ -0,0 +1,12 @@
+package org.example.InterfaceSupplier;
+
+import java.util.function.Supplier;
+
+public class SupplierTest {
+
+    public static void main(String[] args) {
+        Supplier<String> i = () -> "Javier";
+        System.out.println("El valor pasado es: "+ i.get());
+    }
+
+}
