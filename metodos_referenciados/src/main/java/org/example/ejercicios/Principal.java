1946573155de54af8e78bf28aed3ac8445b08d90
Christian <christiancatamuscay@r-fast.com>
2023-02-06 19:41:11 -0500

    27. Introducción a los métodos referenciados(Metodos referenciados)

---
+++ metodos_referenciados/src/main/java/org/example/ejercicios/Principal.java
@@ -0,0 +1,37 @@
+package org.example.ejercicios;
+
+import org.example.modelo.Persona;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        List<Persona> personas =
+                Arrays.asList(
+                        new Persona("Juan",10,"juan@hotmail.com"),
+                        new Persona("Mario",20,"mario@hotmail.com"),
+                        new Persona("Arturo",30,"arturo@hotmail.com"),
+                        new Persona("Maria",40,"maria@hotmail.com"),
+                        new Persona("Beatriz",50,"beatriz@hotmail.com"),
+                        new Persona("Olivia",60,"olivia@hotmail.com"),
+                        new Persona("Angel",70,"angel@hotmail.com"),
+                        new Persona("Omar",80,"omar@hotmail.com"),
+                        new Persona("Pamela",90,"pamela@hotmail.com"),
+                        new Persona("Sadee",25,"sadee@hotmail.com")
+                );
+        System.out.println("Personas sin ordenar: ");
+        personas.forEach(p-> System.out.println(p));
+        System.out.println("");
+        /*
+        Collections.sort(personas,(persona1,persona2)->Persona.compararPorEdad(persona1,persona2));
+        */
+        Collections.sort(personas,Persona::compararPorEdad);
+        System.out.println("Personas ordenadas por edad: ");
+        personas.forEach(p-> System.out.println(p));
+
+    }
+
+}
---
+++ metodos_referenciados/src/main/java/org/example/modelo/Persona.java
@@ -0,0 +1,53 @@
+package org.example.modelo;
+
+public class Persona {
+
+    private String nombre;
+
+    private Integer edad;
+
+    private String email;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Persona(String nombre, int edad, String email) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static int compararPorEdad(Persona a, Persona b){
+        return a.getEdad().compareTo(b.getEdad());
+    }
+}
