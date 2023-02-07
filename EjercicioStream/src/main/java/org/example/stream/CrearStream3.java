f25d1f89e94ba5392e8e77267ac15cc7099f0f74
Christian <christiancatamuscay@r-fast.com>
2023-02-07 10:11:14 -0500

    38. Creación de streams desde colecciones y arreglos(Diferentes maneras de crear streams)

---
+++ EjercicioStream/src/main/java/org/example/stream/CrearStream3.java
@@ -0,0 +1,38 @@
+package org.example.stream;
+
+import org.example.pojo.Estudiante;
+
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class CrearStream3 {
+
+    public static void main(String[] args) {
+
+        IntStream numStream = Arrays.stream(new int[]{1,2,3,4,5,6});
+        numStream.forEach(System.out::println);
+
+        Stream<String> nombres = Arrays.stream(new String[]{"Juan","Pedro","Maria"});
+        nombres.forEach(System.out::println);
+
+        Set<String> lenguajeSet = new HashSet<>();
+        lenguajeSet.add("JAVA");
+        lenguajeSet.add("C++");
+        lenguajeSet.add("C#");
+
+        lenguajeSet.stream().forEach(System.out::println);
+
+        List<Estudiante> estudiantes = new ArrayList<>();
+        estudiantes.add(new Estudiante("Juan",25, 25,25));
+        estudiantes.add(new Estudiante("Mario",25, 25,25));
+        estudiantes.add(new Estudiante("Ramiro",25, 25,25));
+        estudiantes.add(new Estudiante("Esteban",25, 25,25));
+        estudiantes.add(new Estudiante("Maria",25, 25,25));
+
+        Stream<Estudiante> estudianteStream = estudiantes.parallelStream();
+        estudianteStream.forEach(estudiante -> System.out.println(estudiante.getIdentificacion()));
+
+    }
+
+}
