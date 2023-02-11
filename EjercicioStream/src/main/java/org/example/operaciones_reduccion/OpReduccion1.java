b1bcfbc9d06b014efde59edfd8620f0cff964e42
Christian <christiancatamuscay@r-fast.com>
2023-02-10 20:20:41 -0500

    49. Uso de Collect. Parte 2(Operaciones de reduccion mutable)

--- EjercicioStream/src/main/java/org/example/operaciones_reduccion/OpReduccion1.java
+++ EjercicioStream/src/main/java/org/example/operaciones_reduccion/OpReduccion1.java
@@ -2,17 +2,33 @@ package org.example.operaciones_reduccion;
 
 import org.example.pojo.Empleado;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class OpReduccion1 {
 
     public static void main(String[] args) {
-        List<String> nombres = Empleado.empleados()
+       /* List<String> nombres = Empleado.empleados()
                 .stream()
                 .map(Empleado::getNombre)
                 .collect(ArrayList::new,ArrayList::add,ArrayList::addAll
-                );
+                );*/
+
+        /*
+        Set<String> nombres = Empleado.empleados()
+                .stream()
+                .map(Empleado::getNombre)
+                .collect(Collectors.toSet());
+
+        System.out.println("Lista de nombres");
+
+
+         */
+
+        SortedSet<String> nombres = Empleado.empleados()
+                .stream()
+                .map(Empleado::getNombre)
+                .collect(Collectors.toCollection(TreeSet::new) );
 
         System.out.println("Lista de nombres");
         nombres.forEach(System.out::println);
